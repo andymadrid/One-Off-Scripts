61fbdfe85e818486e8dbd81dc1116310ab11694b
Andy Madrid <76069657+andymadrid@users.noreply.github.com>
2025-06-08 22:30:23 -0400

    Update findDMRs.R

--- findDMRs.R
+++ findDMRs.R
@@ -1,5 +1,5 @@
 # A function to find DMRs that is better than the callDMR() function from DSS
-findDMRs <- function(dml, pCutoff = 1e-3, delta = 0, pt.sig = 0.5, minCG = 3, maxGap = 150) {
+findDMRs <- function(dml, pCutoff = 1e-3, delta = 0 minCG = 3, pt.sig = 0.5, maxGap = 150) {
 
   # Identify which CpGs are significant
   dml <- dml %>%
@@ -47,7 +47,6 @@ findDMRs <- function(dml, pCutoff = 1e-3, delta = 0, pt.sig = 0.5, minCG = 3, ma
       end = max(region$pos),
       length = max(region$pos) - min(region$pos),
       nCG = nrow(region),
-      pct.sig = mean(region$isSig),
       diff.Methy = mean(region$diff),
       min.pval = min(region$pval)
     )
